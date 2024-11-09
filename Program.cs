5bb65ae7abadf9b06dda170ef1b4fcccbb923495
JarekTheZegarek <64890467+JarekTheZegarek@users.noreply.github.com>
2024-11-09 20:44:58 +0100

    Update Program.cs - dodałem zobaczymy Czy się dodało w Gicie

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Zobaczymy");
+            Console.WriteLine("Zobaczymy, czy się dodało w Gicie");
         }
     }
 }
